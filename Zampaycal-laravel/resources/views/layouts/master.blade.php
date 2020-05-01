698499aea6fcf697556315d1bf63eead47ba798d
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-05-01 20:28:50 +0200

    Update master.blade.php

--- Zampaycal-laravel/resources/views/layouts/master.blade.php
+++ Zampaycal-laravel/resources/views/layouts/master.blade.php
@@ -191,9 +191,17 @@
     <!-- END: Main Menu-->
 
     <!-- BEGIN: Content-->
-
-
-
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+    <router-view></router-view>
+</div>
+</div>
+</div>
     <!-- END: Content-->
 
     <div class="sidenav-overlay"></div>
@@ -201,7 +209,7 @@
 
     <!-- BEGIN: Footer-->
     <footer class="footer footer-static footer-light">
-        <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2019<a class="text-bold-800 grey darken-2" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent,</a>All rights Reserved</span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i class="feather icon-heart pink"></i></span>
+        <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2019<a class="text-bold-800 grey darken-2" href="https://" target="_blank">ZPC,</a>All rights Reserved</span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i class="feather icon-heart pink"></i></span>
             <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="feather icon-arrow-up"></i></button>
         </p>
     </footer>

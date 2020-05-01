338718059a1fab2f1308ae17140a3cdd8128375e
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-05-01 20:37:03 +0200

    Update

--- Zampaycal-laravel/resources/js/app.js
+++ Zampaycal-laravel/resources/js/app.js
@@ -7,7 +7,9 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue

8d60a6746ccc78cfa5fc428e9bfed64baf8a5c99
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-05-01 20:08:42 +0200

    Update master.blade.php

--- Zampaycal-laravel/resources/views/layouts/master.blade.php
+++ Zampaycal-laravel/resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
-    <title>2 Columns - Vuexy - Bootstrap HTML admin template</title>
+    <title>{{ config('app.name', 'ZAM PAYE CALCULATORE') }}</title>
     <link rel="apple-touch-icon" href="{{asset('asset/app-assets/images/ico/apple-icon-120.png')}}">
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('asset/app-assets/images/ico/favicon.ico')}}">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
